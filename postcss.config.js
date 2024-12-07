5085fa3adb4d1b546f3d6e0770807edc938c30ff
Laura <laura.e.vollmer@gmail.com>
2024-12-06 18:53:12 -0600

    debugging vite and tailwindcss compabilities

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+export default {
+  // Your PostCSS configuration
+  plugins: [
+    require('autoprefixer'),
+    // other plugins
+  ],
+};
