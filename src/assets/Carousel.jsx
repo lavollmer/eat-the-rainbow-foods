79da2751ce94cddb5c2beb31c497bb46200f0f19
Laura <laura.e.vollmer@gmail.com>
2024-12-08 13:35:14 -0600

    carousel

--- src/assets/Carousel.jsx
+++ src/assets/Carousel.jsx
@@ -1,43 +1,9 @@
-import React from "react";
-import { useState } from "react";
-import Orange from "./images/orange.jpg";
+import React from 'react'
 
-const Carousel = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const nextImage = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    );
-  };
-
+const Carousel = () => {
   return (
-    <div className="relative w-full max-w-3xl mx-auto">
-      <div className="overflow-hidden relative h-64">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`absolute inset-0 transition-transform transform ${
-              index === currentIndex ? "translate-x-0" : "translate-x-full"
-            }`}
-          >
-            <img
-              src={image}
-              alt={`Slide ${index}`}
-              className="w-full h-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
-      <button className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-800 text-white p-2">
-        Next
-      </button>
-    </div>
-  );
-};
+    <div>Carousel</div>
+  )
+}
 
-export default Carousel;
+export default Carousel
--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Carousel from './Carousel'
+import Orange from "./images/orange.jpg";
 
 const Header = () => {
+  const images = [Orange];
+
   return (
     <div>
         <h1>Food Slideshow</h1>
-        <Carousel />
+        <Carousel images={image}/>
     </div>
   )
 }
