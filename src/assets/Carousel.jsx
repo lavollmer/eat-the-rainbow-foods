31cca5fef2fdfcaa6fe7b401f685db27f2faf098
Laura <laura.e.vollmer@gmail.com>
2024-12-08 13:56:00 -0600

    working on the carousel section

--- src/assets/Carousel.jsx
+++ src/assets/Carousel.jsx
@@ -10,10 +10,35 @@ const Carousel = () => {
   //creating an array of images
   const images = [Orange];
 
+  const nextSlide = () => {
+    //if index is less than the length of the images array, increment the index by 1
+    if (index < images.length - 1) {
+      setIndex(index + 1);
+    } else {
+      //if index is equal to the length of the images array, set the index to 0
+      setIndex(0);
+    }
+  }
+
+  const prevSlide = () => {
+    //if index is greater than 0, decrement the index by 1
+    if (index > 0) {
+      setIndex(index - 1);
+    } else {
+      //if index is equal to 0, set the index to the length of the images array - 1
+      setIndex(images.length - 1);
+    }
+
 
   return (
-    <div>Carousel</div>
+    <div className="relative w-full max-w-3xl mx-auto">
+      <div className='overflow-hidden relative h-64'>
+        {images.map((images, imageIndex) => {
+        <div></div>
+        )}
+      </div>
+    </div>
   )
 }
 
-export default Carousel
+export default Carousel;
