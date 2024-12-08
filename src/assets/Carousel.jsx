0fc3c16c4a105f53a6920e2b925527c36fa13111
Laura <laura.e.vollmer@gmail.com>
2024-12-08 14:12:48 -0600

    debugging the carousel component

--- src/assets/Carousel.jsx
+++ src/assets/Carousel.jsx
@@ -2,34 +2,24 @@ import React from "react";
 //importing useState from react
 import { useState } from "react";
 
-
-const Carousel = ({images}) => {
+const Carousel = ({ images = [] }) => {
   //using useState to se the index of the image and starting at 0
-  const [index, setIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextSlide = () => {
-    //if index is less than the length of the images array, increment the index by 1
-    if (index < images.length - 1) {
-      setIndex(index + 1);
-    } else {
-      //if index is equal to the length of the images array, set the index to 0
-      setIndex(0);
-    }
+   setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const prevSlide = () => {
-    //if index is greater than 0, decrement the index by 1
-    if (index > 0) {
-      setIndex(index - 1);
-    } else {
-      //if index is equal to 0, set the index to the length of the images array - 1
-      setIndex(images.length - 1);
+   setCurrentIndex(
+    (prevIndex) => (prevIndex - 1 +images.length) % images.length
+   );
     }
 
     return (
       <div className="relative w-full max-w-3xl mx-auto">
         <div className="overflow-hidden relative h-64">
-          {images.map((images, imageIndex) => {
+          {images.map((images, index) => {
             <div
               key={index}
               className={`absolute inset-0 transition-transform transform ${
@@ -38,7 +28,7 @@ const Carousel = ({images}) => {
             >
               <img
                 src={images}
-                alt="food"
+                alt={`Slide-${index}`}
                 className="w-full h-full object-cover"
               />
             </div>;
