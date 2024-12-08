b8c5feb186b0aed647761fd1d0d2749de3cdbe3a
Laura <laura.e.vollmer@gmail.com>
2024-12-08 13:11:48 -0600

    updating footer styling for placeholder

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-row text-sm">
+    <div className="flex flex-row items-center justify-center space-x-10 text-sm bg-blue">
       <div className="flex flex-col">
         <p>Privacy Policy</p>
         <p>Terms of Service</p>
         <p>Support</p>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col font-bold">
         <p>© 2024 Eat The Rainbow Foods Game</p>
       </div>
     </div>
