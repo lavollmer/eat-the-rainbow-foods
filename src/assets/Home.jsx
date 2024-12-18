218c8e091fd8908a1e6761c9e8589a1b7507c830
Laura <laura.e.vollmer@gmail.com>
2024-12-18 14:19:48 -0600

    added button to play game on get started today

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Header from "./Header";
 
 const Home = () => {
+  const handleOnClick = () => {
+    window.location.href = "/play";
+  };
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center">
@@ -11,6 +15,11 @@ const Home = () => {
         <p className="pt-10">
           A great game that helps you eat better, by gamifying your health.
         </p>
+      </div>
+      <div className="flex justify-center pt-10">
+        <button onClick={handleOnClick} className="bg-black text-white rounded-lg px-4 py-4 hover:bg-grey hover:outline-green">
+          Get Started Today!
+        </button>
       </div>
       <Header />
     </div>
