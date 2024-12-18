ce90a60890e6e1fd618bf553b25013e1577a6b89
Laura <laura.e.vollmer@gmail.com>
2024-12-17 18:31:15 -0600

    created a About page section

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Header from "./Header";
+import Footer from "./Footer"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <div>
+            <Header />
+        </div>
+        <div>
+            <h1>Our Story</h1>
+            <p>This is our story.</p>
+        </div>
+        <div>
+            <h1>Meet the Creator</h1>
+            <p>Bio</p>
+        </div>
+        <div>
+            <h1>Our Mission</h1>
+            <p>Mission</p>
+        </div>
+        <div>
+            <Footer />
+        </div>
+    </div>
   )
 }
 
