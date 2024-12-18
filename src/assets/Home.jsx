044abdb2e4d41ecc76a056038582e6703de31c0f
Laura <laura.e.vollmer@gmail.com>
2024-12-18 14:17:18 -0600

    updating the home page with content

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
 const Home = () => {
   return (
     <div>
-        <Header />
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-bold text-3xl">
+          The Easiest Game to Track Healthy Food Eating
+        </h1>
+        <p className="pt-10">
+          A great game that helps you eat better, by gamifying your health.
+        </p>
+      </div>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
